092f7a5096755217274495e911bd8b8272f33934
jonahabraham <jonahabraham@me.com>
2017-10-20 09:25:28 -0700

    i made the eightball do stuff

--- mainscript.js
+++ mainscript.js
@@ -1,10 +1,27 @@
+var list = ["we shall see",
+			"yes" ,
+			"no" ,
+			"maybe" ,
+			"probably not" ,
+			"try again later" ,
+			"doubtful" ,
+			"ask again" ,
+			"as i see it yes" ,
+			"signs point to yes"];
 
-
+function randomNumber () {
+	return Math.floor(Math.random() * 10);
+}
 function testJs(){
 	alert("test!");
 }
 
 function shakeeightball() {
-	document.getElementById(" eightball") .src="eightball2.png"
-alert("we shall see")
+	document.getElementById("eightball").src="eightball2.jpg";
+	//alert("we shall see") ;
+
+	//change paragraph to eightball text
+	document.getElementById("results").innerHTML=list[randomNumber()];
+	
+	alert(randomNumber()) ;
 }
